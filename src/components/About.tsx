85cbd67117408f11702ee185a51cc4fe00685303
Jeevan Vishnu <140521914+jeevanvishnu@users.noreply.github.com>
2025-12-06 14:40:27 +0530

    Change Google Drive link in About component
    Updated the download link to a new Google Drive URL.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -41,7 +41,7 @@ export const About = () => {
               asChild
             >
                 <a
-                href="https://drive.google.com/file/d/1AiYTcf41znO0-h4rYL3HYNkLPXZv5Sp-/view?usp=sharing"
+                href="https://drive.google.com/file/d/1sl2q30GoLP_FZLq0Pa3mjoqwRudXKi9M/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 download
